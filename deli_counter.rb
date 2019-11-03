f65540ec243a8e208870756c6997dca2f4fad31d
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-11-03 20:06:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-      result << " #{index}. #{name}"
+      result << " #{index + 1}. #{name}"
     end
     puts result
   end

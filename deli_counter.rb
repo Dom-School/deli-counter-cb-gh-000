b31453ddc7877080219a84a2b10e29a544017b9c
Dom December <21.domantas.gruodis@azuolynogimnazija.lt>
2019-11-03 19:58:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,3 +4,11 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
+
+def line(katz_deli)
+  result = "The line is currently:"
+  katz_deli.each_with_index do |name, index|
+    result << " #{index}. name"
+  end
+  return result
+end
